3c1d06684662024ac744a27063777679c93b7d5e
Paul Kirschner <p.kirschner01@gmail.com>
2020-01-04 09:02:49 -0500

    another commit.  Progress made. Not a great stopping place but gotta go

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+//set variables
+var dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+//show date and time in header
+$("#currentDay").text(dateTime);
+
+var availHours = [ "9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+ //test array
+ //console.log(availHours[2]);
+
+ //for loop
+ for (var i = 0; i > availHours.length; i++) {
+     var rowsDiv = $("<div>");
+     rowsDiv.addClass("time");
+     rowsDiv.addClass("time" + i);
+     $(".containter").append(rowsDiv);
+
+    rowsDiv.attr("datetime",i + 9);
+    $("div.row0").attr("datetime", "09");
+
+ }
+    //
 
 
 
@@ -21,8 +43,7 @@ $(document).ready(function() {
 
 
 
-
-}
+});
 
 
 
