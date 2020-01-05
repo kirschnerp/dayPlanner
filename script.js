4ef4aaa5a4b58d0b843e4d591dbe937965b4f7b1
Paul Kirschner <p.kirschner01@gmail.com>
2020-01-04 22:08:35 -0500

    more progress

--- script.js
+++ script.js
@@ -5,20 +5,18 @@ var dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 //show date and time in header
 $("#currentDay").text(dateTime);
 
-var availHours = [ "9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var availHours = [ " 9AM ", "10AM", "11AM", "12AM", " 1PM ", " 2PM ", " 3PM ", " 4PM ", " 5PM "];
  //test array
  console.log(availHours[2]);
 
  //for loop
  for (var i = 0; i < availHours.length; i++) {
      var rowsDiv = $("<div>");
-     rowsDiv.addClass("time-block");
+     rowsDiv.addClass("row time-block");
      rowsDiv.addClass("row" + i);
      $(".container").append(rowsDiv);
-     
-
-    rowsDiv.attr("datetime",i + 9);
-    $("div.row0").attr("datetime", "09");
+     rowsDiv.attr("datetime",i + 9);
+     $("div.row0").attr("datetime", "09");
 
     //add rows for times
     var newTimes = $("<div>");
@@ -29,17 +27,33 @@ var availHours = [ "9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5
 //add area for input
 var input = $("<textarea>");
 input.addClass("inputs" + i);
-$(".time" + i).append(input);
+$(".row" + i).append(input);
 
 //add save button
 var saveButton = $("<button>");
-saveButton.addClass("savebtn");
-$(".time" + i).append(saveButton);
-
-
-
+saveButton.addClass("saveBtn");
+saveButton.attr("id", "inputs" + i);
+$(".row" + i).append(saveButton);
 }
-    //
+//function to compare times to add class for css 
+$(".row").each(function(index, element) {
+var pageTime = $(element).attr("datetime");
+var momentTime = moment().format("k");
+console.log(pageTime);
+  console.log(momentTime); 
+ //if statement 
+if (pageTime === momentTime) {
+  $(element).addClass("present");
+} else if (pageTime < momentTime) {
+   $(element).addClass("past");
+ } else if (pageTime > momentTime) {
+    $(element).addClass("future");
+ }
+});
+
+  
+
+
 
 
 
